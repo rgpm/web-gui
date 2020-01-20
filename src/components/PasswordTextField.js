03aacf1b472556cb9fd8b999bc3331c1171c1924
Owen Parkins <oparkins@gmail.com>
2020-01-20 15:18:32 -0700

    feat: The entire password text field is a copy button now

--- src/components/PasswordTextField.js
+++ src/components/PasswordTextField.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import Grid from '@material-ui/core/Grid';
-import { IconButton, Tooltip, Paper } from '@material-ui/core';
+import { IconButton, Tooltip, Paper, Button } from '@material-ui/core';
 import Snackbar from '@material-ui/core/Snackbar';
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  }
+}));
+
 
 export default function PasswordTextField(props) {
 
@@ -15,39 +25,46 @@ export default function PasswordTextField(props) {
     setSnackBarOpen(true);
   }
 
+  const classes = useStyles();
+
   return (
-    <Paper>
-      <Grid
-        container
-        direction="row"
-        justify="flex-start"
-        alignItems="center"
-        wrap="nowrap"
-        spacing={2}
-      >
-        <Grid 
-          item 
+    <Button className={classes.root} onClick={copyPassword}>
+      <Paper className={classes.root}>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          wrap="nowrap"
+          spacing={2}
         >
-          <Tooltip arrow title={"Copy to Clipboard"} >
-            <IconButton  edge="end" onClick={copyPassword}>
-              <FileCopyIcon/>
-            </IconButton>
-          </Tooltip>      
+          <Grid 
+            item 
+          >
+            <Tooltip arrow title={"Copy to Clipboard"} >
+              <IconButton  edge="end" onClick={copyPassword}>
+                <FileCopyIcon/>
+              </IconButton>
+            </Tooltip>      
+          </Grid>
+          <Grid 
+            item 
+            zeroMinWidth
+          >
+            
+              <Typography noWrap>{props.text}</Typography>
+            
+          </Grid>
         </Grid>
-        <Grid 
-          item 
-          zeroMinWidth
-        >
-          <Typography noWrap>{props.text}</Typography>
-        </Grid>
-      </Grid>
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        key={"copySnackBar"}
-        open={snackBarOpen}
-        onClose={() => setSnackBarOpen(false)}
-        message={<span id="message-id">Copied to Clipboard</span>}
-      />
-    </Paper>
+        
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          key={"copySnackBar"}
+          open={snackBarOpen}
+          onClose={() => setSnackBarOpen(false)}
+          message="Copied to Clipboard"
+        />
+      </Paper>
+    </Button>
   );
 }
