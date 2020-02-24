70fceed7f8f5620a25a6d620ada63b0e057d8b00
Owen Parkins <oparkins@gmail.com>
2020-02-24 14:07:47 -0700

    Fixed spelling error

--- src/components/HelpDialog.js
+++ src/components/HelpDialog.js
@@ -120,7 +120,7 @@ export default function HelpDialog(props) {
       </DialogContent>
 
       <DialogTitle>
-        Icon Explaination
+        Icon Explanation
       </DialogTitle>
       <DialogContent>
         <Grid 
